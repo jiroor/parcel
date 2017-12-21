0dea59f3d9143e5f3ca36f7a7fdb05406def857f
jiroor <thx.1by1.or25@gmail.com>
2017-12-21 16:31:49 +0900

    feature: add vue-router

---
+++ src/js/chat.js
@@ -0,0 +1,51 @@
+import * as firebase from 'firebase';
+import _ from 'lodash';
+import Vue from 'vue/dist/vue.js';
+import Vuefire from 'vuefire/dist/vuefire.js';
+
+require('firebase/firestore');
+
+Vue.use(Vuefire);
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyC7VtLhSkzxNzl5jg5pbjSgHnHnHXCjRf4",
+  authDomain: "jiroor-add68.firebaseapp.com",
+  databaseURL: "https://jiroor-add68.firebaseio.com",
+  projectId: "jiroor-add68",
+  storageBucket: "jiroor-add68.appspot.com",
+  messagingSenderId: "774646153392"
+});
+const db = firebase.firestore();
+
+const cols = {
+  messages: db.collection('messages')
+};
+
+module.exports = {
+  data() {
+    return {
+      messageText: '',
+      messages: []
+    }
+  },
+
+  firestore: {
+    messages: cols.messages.orderBy('insert', 'desc')
+  },
+
+  methods: {
+    sendMessage() {
+      if (this.messageText) {
+        cols.messages.add({
+          text: this.messageText,
+          insert: new Date()
+        });
+
+        this.messageText = '';
+      }
+    }
+  },
+
+  template: '#template-page-chat'
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,19 @@
 import '../scss/index.scss';
 
-import * as firebase from 'firebase';
-import _ from 'lodash';
 import Vue from 'vue/dist/vue.js';
-import Vuefire from 'vuefire/dist/vuefire.js';
+import VueRouter from 'vue-router/dist/vue-router.js';
 
-require('firebase/firestore');
+Vue.use(VueRouter);
 
-Vue.use(Vuefire);
+const routes = [{
+  path: '/chat',
+  component: require('./chat.js')
+}];
 
-// Initialize Firebase
-firebase.initializeApp({
-  apiKey: "AIzaSyC7VtLhSkzxNzl5jg5pbjSgHnHnHXCjRf4",
-  authDomain: "jiroor-add68.firebaseapp.com",
-  databaseURL: "https://jiroor-add68.firebaseio.com",
-  projectId: "jiroor-add68",
-  storageBucket: "jiroor-add68.appspot.com",
-  messagingSenderId: "774646153392"
+const router = new VueRouter({
+  routes
 });
-const db = firebase.firestore();
 
-const cols = {
-  messages: db.collection('messages')
-};
-
-new Vue({
-  data() {
-    return {
-      messageText: '',
-      messages: []
-    }
-  },
-
-  firestore: {
-    messages: cols.messages.orderBy('insert', 'desc')
-  },
-
-  methods: {
-    sendMessage() {
-      if (this.messageText) {
-        cols.messages.add({
-          text: this.messageText,
-          insert: new Date()
-        });
-
-        this.messageText = '';
-      }
-    }
-  },
-
-  el: '#app'
-});
+const app = new Vue({
+  router
+}).$mount('#app');
